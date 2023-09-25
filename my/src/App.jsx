55f04d41090f85df0f3aa9c23dd75359367ad11e
Potyomko Veronika <vnpotomko@gmail.com>
2023-09-25 18:40:33 +0300

    дз

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,30 +1,139 @@
 import { Component } from "react";
-import { Modal } from "./components/Modal/Modal";
-// import { GlobalStyle } from "./Global.styled";
-// import TodoList from "./components/TodoList/TodoList";
-// import initialTodos from './todolist.json';
-// import { TodoEditor } from "./components/TodoEditor/TodoEditor";
-// import { Filter } from "./components/Filter/Filter";
-// import { InfoBox } from "./App.styled";
-// import { nanoid } from "nanoid";
+import { nanoid} from 'nanoid';
 
-export class App extends Component{
+export class App extends Component {
   state = {
-    isOpen: false
-  }
-  toggleModal=()=> {
-    this.setState(prevState => ({isOpen: !prevState.isOpen}))
-  }
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+      name: "",
+    number: "",
+    filter: "",
+  };
+
+
+
+  isNameAlreadyExists = (name) => {
+    return this.state.contacts.some((contact) => contact.name === name);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, number } = this.state;
+
+    if (this.isNameAlreadyExists(name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = { id: nanoid(), name, number };
+    this.setState(({ contacts }) => {
+      return { contacts: [...contacts, newContact] };
+    });
+  };
+
+  handleFilter = (e) => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  getVisibleContacts = () => {
+    return this.state.contacts.filter((contact) => {
+     return contact.name.toLowerCase().includes(this.state.filter.toLowerCase());
+    });
+  };
+  deletePhone = phoneid => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter( contacts => contacts.id !== phoneid),
+    }));
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
   render() {
     return (
-      <>
-        <button type="button"onClick={this.toggleModal}>Open</button>
-        {this.state.isOpen && <Modal onClose={ this.toggleModal} />}
-      </>
-    )
+      <div>
+        <div>
+      <h1>Phone book</h1>
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={this.handleChange}
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={this.handleChange}
+        />
+      </label>
+      <button type="submit">Add contact</button>
+    </form>
+</div>
+        <label>
+          Find contacts by name
+          <input onChange={this.handleFilter} type="text" />
+        </label>
+
+        <h2>Contacts</h2>
+        <ul>
+  {this.getVisibleContacts().map(({ id, name, number }) => {
+    return (
+      
+    
+      <li key={id}>
+        <p>
+          {name}: {number}
+        </p>
+        <button type="button" onClick={() => this.props.deletePhone(id)}>
+          Delete
+        </button>
+      </li>
+    );
+  })}
+</ul>
+      </div>
+    );
   }
 }
-
+// const modalRoot = document.querySelector('#modal-root')
+// export class App extends Component{
+//   state = {
+//     isOpen: false
+//   }
+//   closeModal=()=> {
+//     this.setState({isOpen: false})
+//   }
+//   openModal=()=> {
+//     this.setState({isOpen: true})
+//   }
+//   render() {
+//     return (
+//       <>
+        
+//         <button type="button"onClick={this.openModal}>Open</button>
+//         {this.state.isOpen && createPortal(<Modal onClose={ this.closeModal} />, modalRoot)}
+//       </>
+//     )
+//   }
+// }
 // export class App extends Component {
   
 //   state={
--- my/src/components/Modal/Modal.jsx
+++ my/src/components/Modal/Modal.jsx
@@ -3,16 +3,29 @@ import { ModalBackdrop, ModalContent } from "./Modal.styled";
 
 export class Modal extends Component{
     componentDidMount(){
-        window.addEventListener('keydown', (e) => {
-            if (e.code === 'Esape') {
-                this.props.onClose()
+        window.addEventListener('keydown', this.handlerKeyDown)
+    }
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handlerKeyDown)
+    }
+    handlerKeyDown = (e) => {
+            if (e.code === 'Escape') {
+                this.props.onClose();
             }
-        })
+    }
+    handlerBackdropClick = (e) => {
+        console.log(e.target);
+        console.log(e.currentTarget);
+        if (e.target === e.currentTarget) {
+            this.props.onClose()
+        }
     }
     render() {
         return (
-            <ModalBackdrop>
+            <ModalBackdrop onClick={this.handlerBackdropClick}>
                 <ModalContent>
+                    <h1>HI!</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At minus odit aperiam nostrum suscipit, modi ab nemo adipisci facilis saepe recusandae neque voluptates repudiandae quam, repellendus distinctio quas! Repellat, cumque atque accusantium numquam et voluptates quam amet, ducimus ad unde quibusdam vel voluptatibus cum sequi soluta veritatis culpa porro iure.</p>
                     <button type="button"onClick={this.props.onClose}>Close</button>
                 </ModalContent>
             </ModalBackdrop>
